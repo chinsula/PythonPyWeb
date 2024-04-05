0e75f761faf4f22a36e50adc834ac2697a4ee1d3
chins <chinsula81@gmail.com>
2024-04-05 17:18:35 +0500

    lab2 commit 2.2

--- apps/db_train/views.py
+++ apps/db_train/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Max, Count
+from django.db.models import Max, Count, F
 
 from django.shortcuts import render
 from django.views import View
@@ -13,20 +13,21 @@ class TrainView(View):
         self.answer1 = Author.objects.filter(self_esteem=max_self_esteem['max_self_esteem'])
 
         # max_count_entry = Entry.objects.aggregate(max_count_entry=Max(Count('text'))
-        self.answer2 = None #Entry.objects.aggregate(max_count_entries=Max(max_count_entry))  # TODO Какой автор имеет наибольшее количество опубликованных статей?
+        self.answer2 = Author.objects.annotate(num_entries=Count('entries')).order_by('-num_entries')[:1] # TODO Какой автор имеет наибольшее количество опубликованных статей?
 
-        self.answer3 = Entry.objects.filter(tags__name__in=['Кино', 'Здоровье'])  # TODO Какие статьи содержат тег 'Кино' или 'Музыка' ?
+        self.answer3 = Entry.objects.filter(tags__name__in=['Кино', 'Музыка']).distinct()  # TODO Какие статьи содержат тег 'Кино' или 'Музыка' ?
 
-        self.answer4 = None  # TODO Сколько авторов женского пола зарегистрировано в системе?
-        self.answer5 = None  # TODO Какой процент авторов согласился с правилами при регистрации?
-        self.answer6 = None  # TODO Какие авторы имеют стаж от 1 до 5 лет?
+        self.answer4 = Author.objects.filter(gender='ж').count()  # TODO Сколько авторов женского пола зарегистрировано в системе?
 
-        max_age_author = Author.objects.aggregate(max_age_author=Max('age'))
-        self.answer7 = Author.objects.filter(age=max_age_author['max_age_author']) #TODO Какой автор имеет наибольший возраст?
+        self.answer5 = round(Author.objects.filter(status_rule=True).count()*100 / Author.objects.count(), 2)  # TODO Какой процент авторов согласился с правилами при регистрации?
+        self.answer6 = Author.objects.filter(authorprofile__stage__range=(1, 5))  # TODO Какие авторы имеют стаж от 1 до 5 лет?
+        max_age = Author.objects.aggregate(Max('age'))
+        self.answer7 = Author.objects.get(age=max_age['age__max']) #TODO Какой автор имеет наибольший возраст?
 
-        self.answer8 = None  # TODO Сколько авторов указали свой номер телефона?
-        self.answer9 = None  # TODO Какие авторы имеют возраст младше 25 лет?
-        self.answer10 = None  # TODO Сколько статей написано каждым автором?
+        self.answer8 = Author.objects.filter(phone_number__isnull=False).count()  # TODO Сколько авторов указали свой номер телефона?
+        self.answer9 = Author.objects.filter(age__lt=25)  # TODO Какие авторы имеют возраст младше 25 лет?
+        self.answer10 = Entry.objects.values('author__username').annotate(count=Count('id'),
+                                                                          username=F('author__username'))  # TODO Сколько статей написано каждым автором?
 
         context = {f'answer{index}': self.__dict__[f'answer{index}'] for index in range(1, 11)}
 
