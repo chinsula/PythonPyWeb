7d307664c5aa953c9de764e11b191fbee3a8b38d
chins <chinsula81@gmail.com>
2024-04-11 19:14:11 +0500

    Revert "lab3  done commit 1.2"
    This reverts commit 6734681521d9ce86de791858077d8cfa63e5a42c.

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,9 +1,10 @@
+from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.views.decorators.csrf import csrf_exempt  # Чтобы post, put, patch, delete не требовали csrf токена (небезопасно)
 from apps.db_train_alternative.models import Author
-from .serializers import AuthorSerializer, AuthorModelSerializer
+from .serializers import AuthorSerializer
 
 
 class AuthorAPIView(APIView):
@@ -25,7 +26,7 @@ class AuthorAPIView(APIView):
             return Response(serializer.data)
 
     def post(self, request):
-        serializer = AuthorModelSerializer(data=request.data)
+        serializer = AuthorSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -37,7 +38,7 @@ class AuthorAPIView(APIView):
         except Author.DoesNotExist:
             return Response({"message": "Автор не найден"}, status=status.HTTP_404_NOT_FOUND)
 
-        serializer = AuthorModelSerializer(author, data=request.data)
+        serializer = AuthorSerializer(author, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -49,7 +50,11 @@ class AuthorAPIView(APIView):
         except Author.DoesNotExist:
             return Response({"message": "Автор не найден"}, status=status.HTTP_404_NOT_FOUND)
 
-# Create your views here.
+        serializer = AuthorSerializer(author, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         try:
